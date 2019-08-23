f632bb381acecdf337646de6ed32fd40a6587a49
guilhermedeoliveira <contatoguilhermedeoliveira@gmail.com>
2019-08-22 22:14:42 -0300

    feat: initiated google map.

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,15 @@
 import { User } from "./models/User";
 
 const user = new User();
+
+const mapContainer = document.getElementById("map-container");
+
+const mapOptions = {
+  zoom: 1,
+  center: {
+    lat: 0,
+    lng: 0
+  }
+};
+
+new google.maps.Map(mapContainer, mapOptions);
