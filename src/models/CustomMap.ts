6f737ca500ed286fdc81bbcdfbe7d5625049713c
guilhermedeoliveira <contatoguilhermedeoliveira@gmail.com>
2019-08-27 00:38:11 -0300

    feat: rendered info window in markers.

--- src/models/CustomMap.ts
+++ src/models/CustomMap.ts
@@ -19,12 +19,20 @@ export class CustomMap {
   }
 
   addMarker(mappable: Mappable) {
-    new google.maps.Marker({
+    const marker = new google.maps.Marker({
       map: this.googleMap,
       position: {
         lat: mappable.location.lat,
         lng: mappable.location.lng
       }
+    });
+
+    marker.addListener("click", () => {
+      const infoWindow = new google.maps.InfoWindow({
+        content: "Hello World!"
+      });
+
+      infoWindow.open(this.googleMap, marker);
     });
   }
 }
