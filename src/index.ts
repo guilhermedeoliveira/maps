5f6fdd2f23389941f6c510350b2b2f65378632e0
guilhermedeoliveira <contatoguilhermedeoliveira@gmail.com>
2019-08-26 22:26:45 -0300

    feat: rendered customMap markers.

--- src/index.ts
+++ src/index.ts
@@ -5,3 +5,6 @@ import { CustomMap } from "./models/CustomMap";
 const user = new User();
 const company = new Company();
 const customMap = new CustomMap("map-container");
+
+customMap.addMarker(user);
+customMap.addMarker(company);
