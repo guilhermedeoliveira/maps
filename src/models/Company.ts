5629432d196ae69ecb0f64b6438ad40ea7829ee1
guilhermedeoliveira <contatoguilhermedeoliveira@gmail.com>
2019-08-28 00:01:50 -0300

    feat: rendered marker content in Company model.

--- src/models/Company.ts
+++ src/models/Company.ts
@@ -1,8 +1,8 @@
 import faker from "faker";
+import { Mappable } from "./CustomMap";
 
-export class Company {
+export class Company implements Mappable {
   name: string;
-  catchPhrase: string;
   location: {
     lat: number;
     lng: number;
@@ -10,10 +10,18 @@ export class Company {
 
   constructor() {
     this.name = faker.company.companyName();
-    this.catchPhrase = faker.company.catchPhrase();
     this.location = {
       lat: parseFloat(faker.address.latitude()),
       lng: parseFloat(faker.address.longitude())
     };
+  }
+
+  markerContent(): string {
+    const { lat, lng } = this.location;
+
+    return `
+      The Company ${this.name}
+      is at coordinate ${lat} ${lng}
+    `;
   }
 }
