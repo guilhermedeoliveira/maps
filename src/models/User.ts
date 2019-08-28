945046dfddcab514b92f377a8c5ce6e86b5e2518
guilhermedeoliveira <contatoguilhermedeoliveira@gmail.com>
2019-08-27 23:32:06 -0300

    feat: rendered marker content in User model.

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,6 +1,7 @@
 import faker from "faker";
+import { Mappable } from "./CustomMap";
 
-export class User {
+export class User implements Mappable {
   name: string;
   location: {
     lat: number;
@@ -13,5 +14,14 @@ export class User {
       lat: parseFloat(faker.address.latitude()),
       lng: parseFloat(faker.address.longitude())
     };
+  }
+
+  markerContent(): string {
+    const { lat, lng } = this.location;
+
+    return `
+      The user ${this.name}
+      is at coordinate ${lat} ${lng}
+    `;
   }
 }
